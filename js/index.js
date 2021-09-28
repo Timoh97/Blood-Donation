17c9e70e1bc2a23959c10175cdec85f1286607cd
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-09-28 18:20:50 +0300

    add js to allow login

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(".Login").click(function (e) {
+        e.preventDefault();
+        email = $("input#user-email").val();
+        password = $("input#user-password").val();
+
+        if (email == "" || email == null || password == "" || password == null) {
+            $(".display").show().text("Please fill in all details")
+        } else {
+            $(".display").hide();
+            location.replace("book-appointment.html");
+        }
+})
+
+$(".Signup").click(function (e) {
+    e.preventDefault()
+    regName = $("input#UserName").val();
+    regEmail = $("input#Email").val();
+    regPassword = $("input#password").val();
+    passwordConfirm = $("input#confirm-password").val();
+
+    if (regName == "" || regName == null || regEmail == "" || regEmail == null || regPassword == "" ||
+        regPassword == null || passwordConfirm == "" || passwordConfirm == null) {
+        $(".display").show().text("Please fill in all details")
+    } else if (regPassword != passwordConfirm) {
+        $(".display").show().text("Passwords do not match")
+        $("input#password").val("");
+        $("input#confirm-password").val("");
+    } else {
+        $(".display").hide()
+        location.replace("book-appointment.html");
+
+    }
+});
+
+})
